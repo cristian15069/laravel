12d250f097f80b6a9963ed52414b272df68e644b
CRISTIAN ALBERTO GARCIA GARCIA <243271025@alumnos.utn.edu.mx>
2024-12-01 22:21:20 -0600

    task addition implementation controller ,view and model

---
+++ todolist/resources/views/task/index.blade.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <title>To do list</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous" />
+</head>
+
+<body>
+    <header>
+        <!-- place navbar here -->
+    </header>
+    <main>
+        <div class="container">
+            <form action="{{ url('/') }}" method="post">
+                @csrf
+                <div class="mb-3 row">
+                    <label
+                        for="Task"
+                        class="col-4 col-form-label">Task </label>
+                    <div class="col-8">
+                        <input
+                            type="text"
+                            class="form-control"
+                            name="task"
+                            id="task"
+                            placeholder="Write task" />
+                    </div>
+                </div>
+
+                <div class="mb-3 row">
+                    <div class="offset-sm-4 col-sm-8">
+                        <input type="submit" class="btn btn-primary" value="Add task">
+                            
+                    </div>
+                </div>
+            </form>
+        </div>
+
+    </main>
+    <footer>
+        <!-- place footer here -->
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script
+        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
--- todolist/routes/web.php
+++ todolist/routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TaskController::class,'index']);
+Route::post('/', [TaskController::class,'store']);
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
